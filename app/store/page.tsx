b1373a22b0bed29fc1a8dfc5b0d04b2d41c49f88
TheeHk-blip <158249621+TheeHk-blip@users.noreply.github.com>
2024-05-19 00:34:35 +0300

    mods

--- app/store/page.tsx
+++ app/store/page.tsx
@@ -1,17 +1,17 @@
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import { Button } from '@mui/material';
 import Link from 'next/link';
-import { Button, Navbar } from 'react-bootstrap';
+import { Navbar } from 'react-bootstrap';
 export default function Store(){
   return(
   <>
-  <Navbar className="navbar">
+  <Navbar className="navbar" >
     <Link href="/">Home</Link>
     <Link href="/store">Store</Link>
     <Link href="/about">About</Link>
-    <Button style={{width:"3rem", position:"relative"}} variant="outline-primary"
-    className="rounded-circle">
-      <ShoppingCartIcon/>
-      <div>5</div>
+    <Button variant="outlined" style={{marginLeft:"200px"}}>
+    <ShoppingCartIcon/>
+    <div>2</div>
     </Button>
   </Navbar>
     <h1>Welcome Innit!</h1>

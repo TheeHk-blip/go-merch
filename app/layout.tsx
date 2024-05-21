38578c7a7095ffdf320310b7f8cf1d11e9638735
TheeHk-blip <158249621+TheeHk-blip@users.noreply.github.com>
2024-05-21 15:43:08 +0300

    mods

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -18,6 +18,7 @@ export default function RootLayout({children}: { children: React.ReactNode }) {
       <body>
         <Providers>
           {children}
+          <SpeedInsights/>
         </Providers>
       </body>
     </html>
